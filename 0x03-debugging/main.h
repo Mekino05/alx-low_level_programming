cf631abf811229e25c65aed6f359e7fcbf51fa15
princeemekaedeh <pemeka068@gmail.com>
2022-07-09 16:16:26 -0700

    task 2 done

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,25 +1,8 @@
-#include "main.h"
-/**
-*largest_number - returns the largest of 3 numbers
-*@a: first integer
-*@b: second integer
-*@c: third integer
-*Return: largest number
-*/
-int largest_number(int a, int b, int c)
-{
-int largest;
-if (a >= b && a >= c)
-{
-largest = a;
-}
-else if (b >= a && b >= c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
-}
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positíve_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int month, int day);
+#endif
