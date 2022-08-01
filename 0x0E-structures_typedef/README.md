859cb591ebd15f2bfbe0cb4962bff02bfdd337b0
princeemekaedeh <pemeka068@gmail.com>
2022-08-01 10:26:24 -0700

    task 0 done

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,2 @@
-Write a function that initialize a variable of type struct dog prototype; void init_dog(struct dog *d, char *name, float age, char *owner);
+Define a new type struct dog with the following elements; name, type = char * age, type = float
+owner, type = char *
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,21 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef _DOG_H
+#define _DOG_H
 /**
-* main - check the code
-* Return: Always 0.
+* struct dog - Struct dog
+* @name: name
+* @age: age
+* @owner:  owner
+* Description: This struct is for dog
 */
-int main(void)
+struct dog
 {
-struct dog my_dog;
-my_dog.name = "Poppy";
-my_dog.age = 3.5;
-my_dog.owner = "Bob";
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-return (0);
-}
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
