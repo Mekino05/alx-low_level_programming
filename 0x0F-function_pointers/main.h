9f191b64ace2f75be45aeb87b6d1cf82a57ffe5a
princeemekaedeh <pemeka068@gmail.com>
2022-08-03 12:35:19 -0700

    task 0 done

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,22 +1,8 @@
-#ifndef CALC_H
-#define CALC_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-/**
-* struct op - Struct op
-* @op: The operator
-* @f: The function associated
-*/
-typedef struct op
-{
-char *op;
-int (*f)(int a, int b);
-} op_t;
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-#endif
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
+#include <stddef.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif/* FUNCTION_POINTERS_H*/
