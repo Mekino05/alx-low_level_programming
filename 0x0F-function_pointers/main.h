08dcb0f3c4469289e7dc0692c4fc648be8ba132e
princeemekaedeh <pemeka068@gmail.com>
2022-08-03 13:48:20 -0700

    task 0 done

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,8 +1,10 @@
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
-#include <stddef.h>
-int _putchar(char c);
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
+/*
+* Desc: Header file containing prototypes for all functions
+*       used in the 0x0E-function_pointers directory.
+*/
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif
