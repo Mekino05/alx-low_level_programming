651b278c8e8f637bf002d8c618ef54b067f3d1c9
princeemekaedeh <pemeka068@gmail.com>
2022-08-23 14:26:54 -0700

    task 3 done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,45 +1,67 @@
-#include "main.h"
-#include <stddef.h>
+include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
-* _strlen - counts string length
-* @str: string to be used
-* Return: length of the string
+* main - copies the content of one file to another
+* @argc: argument count
+* @argv: arguments passed
+* Return: 1 on success, exit otherwise
 */
-int _strlen(char *str)
+int main(int argc, char *argv[])
 {
-int len = 0;
-while (str[len] != '\0')
-len++;
-return (len);
+int src, dest, n_read = 1024, wrote, close_src, close_dest;
+unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+char buffer[1024];
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+exit(97);
 }
-/**
-* append_text_to_file - appends text at the end of a file
-* @filename: name of the file
-* @text_content: content to be appended
-* Return: 1 on success, -1 otherwise
-*/
-int append_text_to_file(const char *filename, char *text_content)
+src = open(argv[1], O_RDONLY);
+check_IO_stat(src, -1, argv[1], 'O');
+dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+check_IO_stat(dest, -1, argv[2], 'W');
+while (n_read == 1024)
 {
-int file, wrote;
-if (filename == NULL)
-return (-1);
-file = open(filename, O_WRONLY | O_APPEND);
-if (file == -1)
-return (-1);
-if (text_content != NULL)
-{
-wrote = write(file, text_content, _strlen(text_content));
+n_read = read(src, buffer, sizeof(buffer));
+if (n_read == -1)
+check_IO_stat(-1, -1, argv[1], 'O');
+wrote = write(dest, buffer, n_read);
 if (wrote == -1)
-{
-close(file);
-return (-1);
+check_IO_stat(-1, -1, argv[2], 'W');
 }
-close(file);
-return (1);
+close_src = close(src);	check_IO_stat(close_src, src, NULL, 'C');
+close_dest = close(dest);
+check_IO_stat(close_dest, dest, NULL, 'C');
+return (0);
 }
-else
+/**
+* check_IO_stat - checks if a file can be opened or closed
+* @stat: file descriptor of the file to be opened
+* @filename: name of the file
+* @mode: closing or opening
+* @fd: file descriptor
+* Return: void
+*/
+void check_IO_stat(int stat, int fd, char *filename, char mode)
 {
-close(file);
-return (1);
+if (mode == 'C' && stat == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+else if (mode == 'O' && stat == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+exit(98);
+}
+else if (mode == 'W' && stat == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+exit(99);
 }
 }
